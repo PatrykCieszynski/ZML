73146b6e98382b0ae1557c2a8369efede031dc52
PatrykCieszynski <patrykciesz@gmail.com>
2026-08-11 19:21:18 +0200

    feat(ocr): add calibration package

---
+++ apps/ocr-worker/src/zml_ocr_worker/calibration/__init__.py
@@ -0,0 +1,12 @@
+from zml_ocr_worker.calibration.compass import CompassLocator, CompassLocatorConfig
+from zml_ocr_worker.calibration.finder import FinderLocator, FinderLocatorConfig
+from zml_ocr_worker.calibration.model import LocatedCompass, LocatedRegion
+
+__all__ = [
+    "CompassLocator",
+    "CompassLocatorConfig",
+    "FinderLocator",
+    "FinderLocatorConfig",
+    "LocatedCompass",
+    "LocatedRegion",
+]
